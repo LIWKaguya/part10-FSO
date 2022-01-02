01fae12e90c0af5260342af4390b0a4766ad3d88
LIWKaguya <hungle120901@gmail.com>
2022-01-02 06:32:47 +0200

    Exercise 10.5-10.7 (mistake)

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,26 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-import AppBar from './components/AppBar';
-import RepositoryList from './components/RepositoryList';
+import { NativeRouter } from 'react-router-native';
 
-export default function App() {
+import Main from './components/Main';
+
+const App = () => {
   return (
-    <View>
-      <AppBar />
-      <RepositoryList />
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-//
+export default App;
--- rate-repository-app/components/AppBar.jsx
+++ rate-repository-app/components/AppBar.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
-import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
+import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: "black",
-    alignSelf: 'stretch'
+    alignSelf: 'stretch',
+    flexDirection: 'row'
   },
   text: {
     color: "white",
     textAlign: "center",
-    fontWeight: "bold"
+    fontWeight: "bold",
+    margin: 10
   }
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => console.log('hi')}>
-        <Text style={styles.text}>
+      <ScrollView horizontal>
+      <View style={{flex : 1}}>
+        <Link to='/'>
+          <Text style={styles.text}>
             Repositories
-        </Text>
-      </Pressable>
+          </Text>
+        </Link>
+      </View>
+      <View style={{flex: 1}}>
+        <Link to='/login'>
+          <Text style={styles.text}>
+            Login
+          </Text>
+        </Link>
+      </View>
+      </ScrollView>
     </View>
   ); 
 };
